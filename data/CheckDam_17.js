2b72c1f347d1f5a338f7c896a9f95752227ed6a3
csrdthrissur <155517405+csrdthrissur@users.noreply.github.com>
2024-02-26 15:56:25 +0530

    Add files via upload

---
+++ data/CheckDam_17.js
@@ -0,0 +1,1 @@
+var json_CheckDam_17 = {"type":"FeatureCollection","name":"CheckDam_17","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"ThoduName":"Kallachira Thodu","Ward":"6"},"geometry":{"type":"Point","coordinates":[76.640603681385045,9.697186224465829]}}]}
